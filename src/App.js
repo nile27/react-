f77e8d10ba00671557d286d3abb4d6d850d28931
nile27 <ddj03104@gmail.com>
2023-02-26 20:52:59 +0900

    chat_3(state)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Say from './Say';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <>
+    <Counter />
+    <Say />
+    </>
+  );
+}
+
+export default App;
